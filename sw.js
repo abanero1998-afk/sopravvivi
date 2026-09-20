80cb3385045e827dbd6ccdc7562d106a49573976
abanero1998-afk <abanero1998@icloud.com>
2026-09-20 23:26:37 +0200

    Cache v5: include map.css offline

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-const CACHE = "sopravvivi-v4";
-const ASSETS = ["./", "./index.html", "./app.css", "./content.js", "./field.js", "./app.js", "./manifest.json", "./icon.svg"];
+const CACHE = "sopravvivi-v5";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.css",
+  "./map.css",
+  "./content.js",
+  "./field.js",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => c.addAll(ASSETS.filter((a) => a !== "./field.js")))).then(() => self.skipWaiting()));
 });
 self.addEventListener("activate", (event) => {
   event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
