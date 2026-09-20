31ec938b1ed14ff4aa5c5e4efec01489e66ce216
abanero1998-afk <abanero1998@icloud.com>
2026-09-20 22:53:36 +0200

    Aggiunge stile e logica dell'app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sopravvivi-v1";
+const CACHE = "sopravvivi-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,34 +8,19 @@ const ASSETS = [
   "./manifest.json",
   "./icon.svg"
 ];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  if (req.method !== "GET") return;
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(req).then((cached) => {
-      if (cached) return cached;
-      return fetch(req)
-        .then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE).then((cache) => cache.put(req, copy));
-          return res;
-        })
-        .catch(() => caches.match("./index.html"));
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(event.request, copy));
+      return res;
+    }).catch(() => caches.match("./index.html")))
   );
 });
