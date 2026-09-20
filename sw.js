4d681ee2003be76f8eb5f80ac7423ca1990a6ade
abanero1998-afk <abanero1998@icloud.com>
2026-09-20 23:13:38 +0200

    Restyling Apple/ops: icona, tab bar, tema nero, cache v3

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
-const CACHE = "sopravvivi-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.css",
-  "./content.js",
-  "./app.js",
-  "./manifest.json",
-  "./icon.svg"
-];
+const CACHE = "sopravvivi-v3";
+const ASSETS = ["./", "./index.html", "./app.css", "./content.js", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
